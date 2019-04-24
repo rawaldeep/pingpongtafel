76b46222fb286719a2a939cb5f44673cb00b7be2
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-04-24 16:19:40 +0200

    pushes data to atlas mongodb without making multiple copies

--- config/passport.js
+++ config/passport.js
@@ -18,14 +18,24 @@ function(accessToken, refreshToken, profile, done) {
     Name : profile.displayName,
     profile_pic: profile._json.avatar_url
   }
-  const newItem = new User({
-  Github_id: userData.GITHUB_ID,
-  Name: userData.Name,
-  Profile_pic: userData.profile_pic
-  });
-  newItem.save().then(
-    data => console.log(data)
-  );
+  User.findOne({Github_id: userData.GITHUB_ID})
+  .then(data =>{
+    if(!data){
+      const newItem = new User({
+        Github_id: userData.GITHUB_ID,
+        Name: userData.Name,
+        Profile_pic: userData.profile_pic
+        });
+        newItem.save().then(
+          data => console.log(data)
+        );
+        return done(null, profile);
+    }else{
+      console.log('user exists')
+      return done(null, profile);
+    }
+  })
+  
 
   // User.find()
   // .sort({ data: -1})
@@ -36,6 +46,6 @@ function(accessToken, refreshToken, profile, done) {
   // });
 
   // console.log(userData);
-  return done(null, profile);
+  
 }
 ));
