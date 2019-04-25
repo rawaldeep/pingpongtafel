eb5acd194db5d98a3d9d770cdb9a05116064b70a
Rawal Singh <rawaldeepsingh26@gmail.com>
2019-04-25 09:58:53 +0200

    fixed login issue

--- New folder/pingpongtafel-frank/client/app/components/overview/overview.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const overview = () => (
-  <div>
-  <p>welcome to overview</p>
-  </div>
-);
-
-export default overview;
--- New folder/pingpongtafel-frank/config/config.js
+++
@@ -1,7 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  db: 'mongodb+srv://admin:80Cd6957@cluster0-3njq6.mongodb.net/test?retryWrites=true',
-  // db: 'mongodb://localhost:27017/test',
-  db_dev: 'mongodb+srv://admin:80Cd6957@cluster0-3njq6.mongodb.net/test?retryWrites=true', 
-};
